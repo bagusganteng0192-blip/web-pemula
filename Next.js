24ea98e7e8faac80efdc391c434f30b6bd2050e9
bagus <bagusganteng0192@gmail.com>
2026-01-11 02:57:36 +0700

    new updates

--- Next.js
+++ Next.js
@@ -1,1 +1,11 @@
-console.log("halo world");
+  const sound = document.getElementById("clickSound");
+
+  function playSoundOnce() {
+    sound.currentTime = 0; 
+    sound.play();
+  }
+
+  document.querySelectorAll("a, button, summary")
+    .forEach(el => {
+      el.addEventListener("click", playSoundOnce);
+    });
